fefc5ab8ac77254dcf5b91733ceed1935c01fbfe
Aasim Khan <aasimkhan30@gmail.com>
2021-01-07 13:27:10 -0800

    Fixing event name  (#3)
    * adding onChangedEmitter to StubRadioButton
    
    * Added typings for radioButtons onchange event
    
    * Minor version bumb
    
    * Renamed the event to match azdata
    
    * patch v bump
    
    * Fixing proposed api

--- typings/azdata.proposed.d.ts
+++ typings/azdata.proposed.d.ts
@@ -299,7 +299,7 @@ declare module 'azdata' {
 		/**
 		 * An event called when the value of radio button changes
 		 */
-		onChanged: vscode.Event<boolean>;
+		onDidChangeCheckedState: vscode.Event<boolean>;
 	}
 
 	/*
